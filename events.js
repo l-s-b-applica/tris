9267290dec2c24c6f5544a6327efd422aa1ae6fb
l-s-b-applica <lsanbartolome@applica.dev>
2022-11-17 00:10:22 -0300

    Keypress event block (if paused)

--- events.js
+++ events.js
@@ -169,72 +169,64 @@ start.addEventListener('click', playPause)
  /* M O V E M E N T */
 /*******************/
 document.addEventListener('keyup', (e) => {
-    let thisShape = currentShape.rotations[currentRotation]
+    if (playing) {
+        let thisShape = currentShape.rotations[currentRotation]
 
-// LEFT ROTATION
-    if(['Q', 'q'].includes(e.key)) {
-        wipeShape()
-        currentRotation === 0 ? ( currentRotation = currentShape.rotations.length - 1 ) : ( currentRotation-- )
-        whoosh.play()
-        printShape()
-    }
-// RIGHT ROTATION
-    if(['E', 'e'].includes(e.key)) {
-        wipeShape()
-        currentRotation === currentShape.rotations.length - 1 ? ( currentRotation = 0 ) : ( currentRotation++ )
-        whoosh.play()
-        printShape()
-    }
-// LEFT SCROLL
-    if(['A', 'a', 'ArrowLeft'].includes(e.key)) {
-        let notOnLeftLimit = thisShape.filter(
-            tile => leftLimitTriangles.includes(startSpot + variableColumn + tile[0] + tile[2])
-        ).length === 0
-        let noLeftNeighbors = thisShape.filter(
-            tile =>
-            gridTriangles[startSpot + variableColumn + tile[0] + tile[2] - 2]?.hasAttribute('frozen')
-        ).length === 0
-
-        if(notOnLeftLimit && noLeftNeighbors) {
+    // LEFT ROTATION
+        if(['Q', 'q'].includes(e.key)) {
             wipeShape()
-            variableColumn -= 2
+            currentRotation === 0 ? ( currentRotation = currentShape.rotations.length - 1 ) : ( currentRotation-- )
+            whoosh.play()
             printShape()
-        } else {
-            knock.play()
         }
-    }
-// RIGHT SCROLL
-    if(['D', 'd', 'ArrowRight'].includes(e.key)) {
-        let notOnRightLimit = thisShape.filter(
-            tile => rightLimitTriangles.includes(startSpot + variableColumn + tile[0] + tile[2])
-        ).length === 0
-        let noRightNeighbors = thisShape.filter(
-            tile => gridTriangles[startSpot + variableColumn + tile[0] + tile[2] + currentFall[tile[1] + currentBaseRow]] &&
-            gridTriangles[startSpot + variableColumn + tile[0] + tile[2] + 2].hasAttribute('frozen')
-        ).length === 0
-
-        if(notOnRightLimit && noRightNeighbors) {
+    // RIGHT ROTATION
+        if(['E', 'e'].includes(e.key)) {
             wipeShape()
-            variableColumn += 2
+            currentRotation === currentShape.rotations.length - 1 ? ( currentRotation = 0 ) : ( currentRotation++ )
+            whoosh.play()
             printShape()
-        } else {
-            knock.play()
         }
-    }
-// VERTICAL SCROLL (For testing purposes)
-// UP SCROLL
-    if(['W', 'w', 'ArrowUp'].includes(e.key)) {
-        if (verticalFallEnabled) { limitSwitch() }
-        if (
-            thisShape.filter(
-                tile => topLimitTriangles.includes(startSpot + variableColumn + tile[0] + tile[2])
+    // LEFT SCROLL
+        if(['A', 'a', 'ArrowLeft'].includes(e.key)) {
+            let notOnLeftLimit = thisShape.filter(
+                tile => leftLimitTriangles.includes(startSpot + variableColumn + tile[0] + tile[2])
             ).length === 0
-        ) { moveUp() }
-    }
-// DOWN SCROLL
-    if(['ArrowDown', 's', 'S'].includes(e.key)) {
-        if (verticalFallEnabled) { limitSwitch() }
-        moveDown()
+            let noLeftNeighbors = thisShape.filter(
+                tile =>
+                gridTriangles[startSpot + variableColumn + tile[0] + tile[2] - 2]?.hasAttribute('frozen')
+            ).length === 0
+
+            if(notOnLeftLimit && noLeftNeighbors) {
+                wipeShape()
+                variableColumn -= 2
+                printShape()
+            } else {
+                knock.play()
+            }
+        }
+    // RIGHT SCROLL
+        if(['D', 'd', 'ArrowRight'].includes(e.key)) {
+            let notOnRightLimit = thisShape.filter(
+                tile => rightLimitTriangles.includes(startSpot + variableColumn + tile[0] + tile[2])
+            ).length === 0
+            let noRightNeighbors = thisShape.filter(
+                tile => gridTriangles[startSpot + variableColumn + tile[0] + tile[2] + currentFall[tile[1] + currentBaseRow]] &&
+                gridTriangles[startSpot + variableColumn + tile[0] + tile[2] + 2].hasAttribute('frozen')
+            ).length === 0
+
+            if(notOnRightLimit && noRightNeighbors) {
+                wipeShape()
+                variableColumn += 2
+                printShape()
+            } else {
+                knock.play()
+            }
+        }
+    // DOWN SCROLL
+        if(['ArrowDown', 's', 'S'].includes(e.key)) {
+            if (verticalFallEnabled) { limitSwitch() }
+            moveDown()
+        }
     }
   });
   
