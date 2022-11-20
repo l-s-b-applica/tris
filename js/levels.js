aef808d8722ab0c2f717bffd9fa477ae0ec89e13
Lucio SB <95644016+l-s-b-applica@users.noreply.github.com>
2022-11-20 17:31:49 -0300

    Somewhat stable version for public use (#9)
    * Swag gifs for future use at splash screen
    
    * js files into new js folder. Level features
    
    * Simplifying svg shape styles
    
    * home screen init
    
    * level length tweak
    
    * play button gif
    
    * css folder
    
    * Shapes marquee, game instructions
    
    * Home screen and some adjustments
    
    * level logic fix
    
    * Home screen additions, in game style fixes, text changes

--- js/levels.js
+++ js/levels.js
@@ -9,19 +9,14 @@ function clearBoard() {
 
 function levelCheck() {
     if(level === 1 && score > 4000) {
-
         playPause()
         level2()
         newLevel = true
-        clearBoard()
-        setTimeout(() => {playPause()}, 1500)
     }
     if(level === 2 && score > 8000) {
         playPause()
         level3()
         newLevel = true
-        clearBoard()
-        setTimeout(() => {playPause()}, 1500)
     }
     if(level === 3 && score > 10000) {
         playPause()
@@ -32,15 +27,23 @@ function levelCheck() {
 
 function level2() {
     wipeShape()
+    clearBoard()
     level = 2
     alert('Nivel 2')
     enableRightFall()
+    restartShapePosition()
+    newShape()
+    playPause()
 }
 
 function level3() {
     wipeShape()
+    clearBoard()
     level = 3
     alert(`Nivel 3
     A ver ahora de qué te vas a disfrazar.`)
     enableVerticalFall()
+    restartShapePosition()
+    newShape()
+    playPause()
 }
